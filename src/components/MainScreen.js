25223e2e3ebc41c5e228a60158a13b018db4ff4a
Alex Sánchez C <asmalexander@hotmail.com>
2023-08-14 02:57:36 -0600

    Funcionalidad buscar peliculas
    Se agrego la funcionalidad de la tabla de resultados junto con la pantalla emergente de detalles, ademas de un Navbar

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
 import './style/components.css';
+import NavBar from './NavBar';
 
 function MainScreen() {
     return(
         <div>
             <div className="principalFont">
+                <NavBar/>
                 <div className="moviesSquare"></div>
                 <div className="crudSquare">
                     <h1 style={{position:'absolute',top:"50px"}}>GESTOR DE PELICULAS</h1>
                     <Link to="http://localhost:3000/buscarpeliculas">
-                        <button className="buttons" style={{top:"250px", left:"200px"}}  >BUSCAR PELICULAS</button>
+                        <button className="buttons" style={{top:"250px", left:"200px"}}>BUSCAR PELICULAS</button>
                     </Link>
                     
                     <Link to="http://localhost:3000/agregarpeliculas">
@@ -25,7 +27,6 @@ function MainScreen() {
                     </Link>
                     
                 </div>
-                <div className="logo"></div>
             </div>
         </div>
     );
